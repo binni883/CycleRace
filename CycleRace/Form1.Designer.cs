7595131802716e324e6fcb70e4c6a7084adff6ed
binni883@gmail.com <binni883@gmail.com>
2021-06-02 12:40:26 +0300

    Add project files.

---
+++ CycleRace/Form1.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace CycleRace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.racer4 = new System.Windows.Forms.PictureBox();
+            this.racer3 = new System.Windows.Forms.PictureBox();
+            this.racer2 = new System.Windows.Forms.PictureBox();
+            this.racer1 = new System.Windows.Forms.PictureBox();
+            this.cycleTrack = new System.Windows.Forms.PictureBox();
+            this.btnRace = new System.Windows.Forms.Button();
+            this.buttonBets = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.MinimumBet = new System.Windows.Forms.Label();
+            this.labelAlsBet = new System.Windows.Forms.Label();
+            this.labelBobsBet = new System.Windows.Forms.Label();
+            this.labelJoesBet = new System.Windows.Forms.Label();
+            this.numericUpDownCar = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownBets = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButtonKamal = new System.Windows.Forms.RadioButton();
+            this.radioButtonSunny = new System.Windows.Forms.RadioButton();
+            this.radioButtonBinni = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.racer4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racer3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cycleTrack)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBets)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.racer4);
+            this.panel1.Controls.Add(this.racer3);
+            this.panel1.Controls.Add(this.racer2);
+            this.panel1.Controls.Add(this.racer1);
+            this.panel1.Controls.Add(this.cycleTrack);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1199, 491);
+            this.panel1.TabIndex = 0;
+            // 
+            // racer4
+            // 
+            this.racer4.Image = global::CycleRace.Properties.Resources._1___Copy2;
+            this.racer4.Location = new System.Drawing.Point(41, 328);
+            this.racer4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.racer4.Name = "racer4";
+            this.racer4.Size = new System.Drawing.Size(133, 62);
+            this.racer4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.racer4.TabIndex = 5;
+            this.racer4.TabStop = false;
+            // 
+            // racer3
+            // 
+            this.racer3.Image = global::CycleRace.Properties.Resources._1___Copy2;
+            this.racer3.Location = new System.Drawing.Point(41, 236);
+            this.racer3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.racer3.Name = "racer3";
+            this.racer3.Size = new System.Drawing.Size(133, 62);
+            this.racer3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.racer3.TabIndex = 4;
+            this.racer3.TabStop = false;
+            // 
+            // racer2
+            // 
+            this.racer2.Image = global::CycleRace.Properties.Resources._1___Copy2;
+            this.racer2.Location = new System.Drawing.Point(41, 140);
+            this.racer2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.racer2.Name = "racer2";
+            this.racer2.Size = new System.Drawing.Size(133, 62);
+            this.racer2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.racer2.TabIndex = 3;
+            this.racer2.TabStop = false;
+            this.racer2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // racer1
+            // 
+            this.racer1.Image = global::CycleRace.Properties.Resources._1___Copy2;
+            this.racer1.Location = new System.Drawing.Point(41, 53);
+            this.racer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.racer1.Name = "racer1";
+            this.racer1.Size = new System.Drawing.Size(133, 62);
+            this.racer1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.racer1.TabIndex = 2;
+            this.racer1.TabStop = false;
+            // 
+            // cycleTrack
+            // 
+            this.cycleTrack.Image = global::CycleRace.Properties.Resources.track_3_copy;
+            this.cycleTrack.Location = new System.Drawing.Point(-3, -2);
+            this.cycleTrack.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cycleTrack.Name = "cycleTrack";
+            this.cycleTrack.Size = new System.Drawing.Size(1200, 492);
+            this.cycleTrack.TabIndex = 1;
+            this.cycleTrack.TabStop = false;
+            // 
+            // btnRace
+            // 
+            this.btnRace.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRace.Location = new System.Drawing.Point(765, 505);
+            this.btnRace.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRace.Name = "btnRace";
+            this.btnRace.Size = new System.Drawing.Size(123, 39);
+            this.btnRace.TabIndex = 38;
+            this.btnRace.Text = "Race!";
+            this.btnRace.UseVisualStyleBackColor = true;
+            this.btnRace.Click += new System.EventHandler(this.btnRace_Click);
+            // 
+            // buttonBets
+            // 
+            this.buttonBets.Location = new System.Drawing.Point(171, 500);
+            this.buttonBets.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonBets.Name = "buttonBets";
+            this.buttonBets.Size = new System.Drawing.Size(149, 52);
+            this.buttonBets.TabIndex = 37;
+            this.buttonBets.Text = "Place Bet";
+            this.buttonBets.UseVisualStyleBackColor = true;
+            this.buttonBets.Click += new System.EventHandler(this.buttonBets_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.MinimumBet);
+            this.groupBox1.Controls.Add(this.labelAlsBet);
+            this.groupBox1.Controls.Add(this.labelBobsBet);
+            this.groupBox1.Controls.Add(this.labelJoesBet);
+            this.groupBox1.Controls.Add(this.numericUpDownCar);
+            this.groupBox1.Controls.Add(this.numericUpDownBets);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.radioButtonKamal);
+            this.groupBox1.Controls.Add(this.radioButtonSunny);
+            this.groupBox1.Controls.Add(this.radioButtonBinni);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(19, 562);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(1149, 158);
+            this.groupBox1.TabIndex = 36;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Betting System";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // MinimumBet
+            // 
+            this.MinimumBet.AutoSize = true;
+            this.MinimumBet.Location = new System.Drawing.Point(324, 47);
+            this.MinimumBet.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.MinimumBet.Name = "MinimumBet";
+            this.MinimumBet.Size = new System.Drawing.Size(99, 17);
+            this.MinimumBet.TabIndex = 26;
+            this.MinimumBet.Text = "Minimum Bet";
+            // 
+            // labelAlsBet
+            // 
+            this.labelAlsBet.AutoSize = true;
+            this.labelAlsBet.BackColor = System.Drawing.Color.White;
+            this.labelAlsBet.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelAlsBet.Location = new System.Drawing.Point(705, 130);
+            this.labelAlsBet.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelAlsBet.Name = "labelAlsBet";
+            this.labelAlsBet.Size = new System.Drawing.Size(184, 19);
+            this.labelAlsBet.TabIndex = 25;
+            this.labelAlsBet.Text = "Kamal hasn\'t placed bet";
+            // 
+            // labelBobsBet
+            // 
+            this.labelBobsBet.AutoSize = true;
+            this.labelBobsBet.BackColor = System.Drawing.Color.White;
+            this.labelBobsBet.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelBobsBet.Location = new System.Drawing.Point(705, 81);
+            this.labelBobsBet.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelBobsBet.Name = "labelBobsBet";
+            this.labelBobsBet.Size = new System.Drawing.Size(185, 19);
+            this.labelBobsBet.TabIndex = 24;
+            this.labelBobsBet.Text = "Sunny hasn\'t placed bet";
+            // 
+            // labelJoesBet
+            // 
+            this.labelJoesBet.AutoSize = true;
+            this.labelJoesBet.BackColor = System.Drawing.Color.White;
+            this.labelJoesBet.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelJoesBet.Location = new System.Drawing.Point(705, 34);
+            this.labelJoesBet.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelJoesBet.Name = "labelJoesBet";
+            this.labelJoesBet.Size = new System.Drawing.Size(176, 19);
+            this.labelJoesBet.TabIndex = 23;
+            this.labelJoesBet.Text = "Binni hasn\'t placed bet";
+            // 
+            // numericUpDownCar
+            // 
+            this.numericUpDownCar.Location = new System.Drawing.Point(549, 91);
+            this.numericUpDownCar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDownCar.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDownCar.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownCar.Name = "numericUpDownCar";
+            this.numericUpDownCar.Size = new System.Drawing.Size(59, 23);
+            this.numericUpDownCar.TabIndex = 22;
+            this.numericUpDownCar.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownBets
+            // 
+            this.numericUpDownBets.Location = new System.Drawing.Point(549, 44);
+            this.numericUpDownBets.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDownBets.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownBets.Name = "numericUpDownBets";
+            this.numericUpDownBets.Size = new System.Drawing.Size(59, 23);
+            this.numericUpDownBets.TabIndex = 21;
+            this.numericUpDownBets.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(516, 100);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 17);
+            this.label2.TabIndex = 20;
+            this.label2.Text = " #";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(523, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 17);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "$";
+            // 
+            // radioButtonKamal
+            // 
+            this.radioButtonKamal.AutoSize = true;
+            this.radioButtonKamal.Location = new System.Drawing.Point(45, 130);
+            this.radioButtonKamal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButtonKamal.Name = "radioButtonKamal";
+            this.radioButtonKamal.Size = new System.Drawing.Size(73, 21);
+            this.radioButtonKamal.TabIndex = 2;
+            this.radioButtonKamal.TabStop = true;
+            this.radioButtonKamal.Text = "Kamal";
+            this.radioButtonKamal.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonSunny
+            // 
+            this.radioButtonSunny.AutoSize = true;
+            this.radioButtonSunny.Location = new System.Drawing.Point(45, 79);
+            this.radioButtonSunny.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButtonSunny.Name = "radioButtonSunny";
+            this.radioButtonSunny.Size = new System.Drawing.Size(74, 21);
+            this.radioButtonSunny.TabIndex = 1;
+            this.radioButtonSunny.TabStop = true;
+            this.radioButtonSunny.Text = "Sunny";
+            this.radioButtonSunny.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonBinni
+            // 
+            this.radioButtonBinni.AutoSize = true;
+            this.radioButtonBinni.Location = new System.Drawing.Point(45, 34);
+            this.radioButtonBinni.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButtonBinni.Name = "radioButtonBinni";
+            this.radioButtonBinni.Size = new System.Drawing.Size(65, 21);
+            this.radioButtonBinni.TabIndex = 0;
+            this.radioButtonBinni.TabStop = true;
+            this.radioButtonBinni.Text = "Binni";
+            this.radioButtonBinni.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1203, 743);
+            this.Controls.Add(this.btnRace);
+            this.Controls.Add(this.buttonBets);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.racer4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racer3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cycleTrack)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBets)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox cycleTrack;
+        private System.Windows.Forms.PictureBox racer4;
+        private System.Windows.Forms.PictureBox racer3;
+        private System.Windows.Forms.PictureBox racer2;
+        private System.Windows.Forms.PictureBox racer1;
+        private System.Windows.Forms.Button btnRace;
+        private System.Windows.Forms.Button buttonBets;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label MinimumBet;
+        private System.Windows.Forms.Label labelAlsBet;
+        private System.Windows.Forms.Label labelBobsBet;
+        private System.Windows.Forms.Label labelJoesBet;
+        private System.Windows.Forms.NumericUpDown numericUpDownCar;
+        private System.Windows.Forms.NumericUpDown numericUpDownBets;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButtonKamal;
+        private System.Windows.Forms.RadioButton radioButtonSunny;
+        private System.Windows.Forms.RadioButton radioButtonBinni;
+    }
+}
+
---
+++ CycleRace/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CycleRace
+{
+    public partial class Form1 : Form
+    {
+        //Creating the objects of classes
+        TrackSetup[] cycles = new TrackSetup[4];//instance of tracksetup class
+        ClientClass[] guys = new ClientClass[3];//object of client class
+        public Form1()
+        {
+            InitializeComponent();
+            RaceTrackSetting();//calling the set race track funtion
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonBets_Click(object sender, EventArgs e)
+        {
+            int GuyNumber = 0;
+
+            if (radioButtonBinni.Checked)
+            {
+                GuyNumber = 0;//when radio button bikram is checked then set its id is 0
+            }
+            if (radioButtonSunny.Checked)
+            {
+                GuyNumber = 1;//when radio button sunny is checked then set its id is 1
+            }
+            if (radioButtonKamal.Checked)
+            {
+                GuyNumber = 2;//when radio button kamal is checked then set its id is 2
+            }
+
+            guys[GuyNumber].PlaceBet((int)numericUpDownBets.Value, (int)numericUpDownCar.Value);//when any radio button is checked then place bet function is called and bet is placed and show amount and car number
+            guys[GuyNumber].UpdateLabels();//with this code line the labels are updated
+        }
+
+        private void btnRace_Click(object sender, EventArgs e)
+        {
+            CycleRaceStart();//calling the function of cycle race start with this function when we click on the race button the race will start
+        }
+        private void RaceTrackSetting()//this funtion is for setting the race track
+        {
+            MinimumBet.Text = string.Format("Minimum Bet $1", (int)numericUpDownBets.Minimum);//Showing the minimum bet rate in label
+
+            int startingPosition = racer1.Right - cycleTrack.Left; //Setting the variable for starting position from cycle 1
+            int raceTrackLength = cycleTrack.Size.Width;//Setting the variable of length of cycletrack
+
+            //Setting values of the array of the class greyhound for racing for the first part of the game as suggested in assignment
+            cycles[0] = new TrackSetup()
+            {
+                MyPictureBox = racer1,
+                RacetrackLength = raceTrackLength,
+                StartingPosition = startingPosition
+            };
+
+            cycles[1] = new TrackSetup()
+            {
+                MyPictureBox = racer2,
+                RacetrackLength = raceTrackLength,
+                StartingPosition = startingPosition
+            };
+            cycles[2] = new TrackSetup()
+            {
+                MyPictureBox = racer3,
+                RacetrackLength = raceTrackLength,
+                StartingPosition = startingPosition
+            };
+            cycles[3] = new TrackSetup()
+            {
+                MyPictureBox = racer4,
+                RacetrackLength = raceTrackLength,
+                StartingPosition = startingPosition
+            };
+
+            //this part is for assigning the constructor values which is created in guy class
+            guys[0] = new ClientClass("Binni", null, 60, radioButtonBinni, labelJoesBet);
+            guys[1] = new ClientClass("Sunny", null, 85, radioButtonSunny, labelBobsBet);
+            guys[2] = new ClientClass("Kamal", null, 55, radioButtonKamal, labelAlsBet);
+
+            foreach (ClientClass guy in guys)
+            {
+                guy.UpdateLabels();//using the foreach loop for assigning the values of labels for bet
+            }
+        }
+
+        private void CycleRaceStart()//this is function for starting the race
+        {
+            bool NoWinner = true;
+            int winningCycle;
+            btnRace.Enabled = false;//Button will be false whenever race is continue
+
+            while (NoWinner)
+            {
+                Application.DoEvents();
+                for (int i = 0; i < cycles.Length; i++)//loop start and it will continue whenever length of tracksetup class or track is not finished
+                {
+                    Thread.Sleep(1);//sleep function for the speed of cars
+                    if (cycles[i].Run())//here run function is called from greyhound class for running the cycle and condition is checked for three random cycles
+                    {
+                        winningCycle = i + 1;
+                        NoWinner = false;
+                        MessageBox.Show("We have a winner - Cycle #" + winningCycle);
+                        foreach (ClientClass guy in guys)
+                        {
+                            if (guy.MyBet != null)//condition is checked for betting
+                            {
+                                guy.Collect(winningCycle);
+                                guy.MyBet = null;
+                                guy.UpdateLabels();
+                            }
+                        }
+
+                        foreach (TrackSetup cycle in cycles)
+                        {
+                            cycle.TakeStartingPosition();
+                        }
+
+                        break;
+                    }
+                }
+
+            }
+
+            btnRace.Enabled = true;//here race button is enabled when race is finished
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
